099f60710b516fdda6f9ff8ea021c00caf34f971
m-abdullah455 <commandocommando828@gmail.com>
2026-06-13 20:51:37 +0500

    ---

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,22 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-
-      <a href="/">Toggle</a>
-      <Link to="/">Toggle</Link>
-    </div>
+    <>
+        <div className='navbar bg-blue-900 p-3 text-white flex justify-between'>
+              <div>
+                Logo
+              </div>
+              <div>
+                <ul className='flex gap-4'>
+                  <li> <Link to="/abc">Slide</Link> </li>
+                  <li>  <Link to="/counter">Counter</Link> </li>
+                </ul>
+              </div>
+              <div>
+                <button>Toggle</button>
+              </div>
+        </div>
+    </>
   )
 }
 
